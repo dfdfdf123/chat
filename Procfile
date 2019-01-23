914d34fe0f84ac4ff82ad9f8841400ed988ea17d
johnromanoff <johnromanoff@yandex.ru>
2019-01-24 01:20:22 +0300

    init

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JVM_OPTS -cp target/todo-app.jar clojure.main -m todo-app.server
--- env/prod/cljs/todo_app/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns todo-app.prod
-  (:require [todo-app.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
